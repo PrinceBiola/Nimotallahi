da87f689e2bd5b0ba395604bfbbba16cb19c08ab
PrinceBiola <abiolaoladapo2004@gmail.com>
2025-01-13 10:48:32 +0100

    1@

--- src/Pages/Auth/AdminLogin.jsx
+++ src/Pages/Auth/AdminLogin.jsx
@@ -1,24 +1,21 @@
-import React, { useState } from 'react'
-import NavWrapper from '../../Components/NavWrapper'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import NavWrapper from "../../Components/NavWrapper";
+import { Link, useNavigate } from "react-router-dom";
 
 function AdminLogin() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  
- 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <NavWrapper>
       <div className="container mx-auto p-4">
         <h2 className="text-2xl font-bold mb-4">Admin Login</h2>
-        <form  className="space-y-4">
+        <form className="space-y-4">
           <div>
             <label className="block text-sm font-medium">Email</label>
             <input
               type="email"
               value={email}
-              
               required
               className="mt-1 block w-full border border-gray-300 rounded-md p-2"
             />
@@ -28,21 +25,22 @@ function AdminLogin() {
             <input
               type="password"
               value={password}
-              
               required
-              className="mt-1 block w-full border border-gray-300 rounded-md p-2"
+              className="mt-1 block w-full border border-gray-300 rounded-md p-2 mb-4"
             />
           </div>
-          <button
-            type="submit"
-            className="w-full flex items-center justify-center px-4 py-2 text-sm font-medium bg-blue-900 text-white hover:bg-blue-800 rounded-md transition-colors"
-          >
-            Login
-          </button>
+          <Link to="/admin">
+            <button
+              type="submit"
+              className="w-full flex items-center justify-center px-4 py-2 text-sm font-medium bg-blue-900 text-white hover:bg-blue-800 rounded-md transition-colors"
+            >
+              Login
+            </button>
+          </Link>
         </form>
       </div>
     </NavWrapper>
   );
 }
 
-export default AdminLogin
+export default AdminLogin;
